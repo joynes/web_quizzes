75bcecb38a2d111e1226c5db20274c71f153228f
Johannes Kahlare <joynez11@gmail.com>
2023-06-03 08:46:15 +0200

    Support for different levels

--- quiz.js
+++ quiz.js
@@ -13,7 +13,8 @@ let questions2 = [
     // add more questions as needed
 ];
 
-let questions = [
+let stages = [
+  [
     {
         question: "Vilka tre områden samverkar för att forma och styra samhället?",
         options: ["Politik, ekonomi, och kultur", "Politik, religion, och vetenskap", "Ekonomi, vetenskap, och kultur", "Politik, vetenskap, och kultur"],
@@ -255,17 +256,219 @@ answer: 1
     answer: 0
 }
    
+  ],
+  [
+
+
+    {
+        "question": "Vad betyder ordet demokrati?",
+        "options": ["Kunglig makt", "Folkets makt", "Presidentens makt", "Partiets makt"],
+        "answer": 1
+    },
+    {
+        "question": "Vad innebär det att all makt utgår ifrån folket? Och i vilken lag finns den formuleringen?",
+        "options": ["Alla beslut fattas av befolkningen, i Föreningslagen", "Alla beslut fattas av befolkningen, i Regeringsformen", "Regeringen bestämmer allt, i Regeringsformen", "Regeringen bestämmer allt, i Föreningslagen"],
+        "answer": 1
+    },
+    {
+        "question": "Varför är demokrati en färskvara?",
+        "options": ["Eftersom den kan bli föråldrad", "Eftersom den måste underhållas och skyddas", "Eftersom den kan ätas upp", "Eftersom den kan säljas"],
+        "answer": 1
+    },
+    {
+        "question": "Hur styrs Sverige?",
+        "options": ["Genom en diktator", "Genom en monark", "Genom en demokrati", "Genom en oligarki"],
+        "answer": 2
+    },
+    {
+        "question": "Vad innebär representativ parlamentarism?",
+        "options": ["Folket röstar direkt på lagförslag", "Folket röstar in företrädare som fattar beslut å deras vägnar", "Regeringen väljs av monarken", "Regeringen utses av ett utvalt råd"],
+        "answer": 1
+    },
+
+    {
+        "question": "Hur ser rollerna och maktfördelningen ut mellan politiker – tjänstemän – medborgare - massmedia?",
+        "options": ["Politiker bestämmer allt, tjänstemän utför, medborgare röstar, massmedia rapporterar", "Tjänstemän bestämmer allt, politiker utför, medborgare röstar, massmedia rapporterar", "Medborgare bestämmer allt, politiker utför, tjänstemän övervakar, massmedia rapporterar", "Massmedia bestämmer allt, politiker utför, tjänstemän övervakar, medborgare röstar"],
+        "answer": 0
+    },
+    {
+        "question": "Vilka är regeringens främsta uppgifter?",
+        "options": ["Stifta lagar, verkställa domar", "Stifta lagar, verkställa lagar", "Verkställa lagar, föreslå lagar", "Föreslå lagar, utöva tillsyn"],
+        "answer": 2
+    },
+    {
+        "question": "Vilka är riksdagens främsta uppgifter?",
+        "options": ["Verkställa lagar, föreslå lagar", "Stifta lagar, verkställa lagar", "Stifta lagar, kontrollera regeringen", "Föreslå lagar, utöva tillsyn"],
+        "answer": 2
+    },
+    {
+        "question": "Vilken roll har vår kung/kungafamilj?",
+        "options": ["Styra landet", "Representera landet och har ceremoniella uppgifter", "Bestämma lagar", "Verkställa lagar"],
+        "answer": 1
+    },
+    {
+        "question": "Vilka olika typer av domstolar finns det, och vilka är deras respektive uppgifter?",
+        "options": ["Tingsrätt, hovrätt, Högsta domstolen - att döma i brottmål", "Tingsrätt, hovrätt, Högsta domstolen - att utföra lagar", "Tingsrätt, hovrätt, Högsta domstolen - att föreslå lagar", "Tingsrätt, hovrätt, Högsta domstolen - att kontrollera lagar"],
+        "answer": 0
+    },
+    {
+        "question": "Hur länge har vi haft en offentlig förvaltning i Sverige?",
+        "options": ["Sedan 1900-talet", "Sedan 1800-talet", "Sedan 1700-talet", "Sedan medeltiden"],
+        "answer": 3
+    },
+    {
+        "question": "Vad kännetecknar offentlig förvaltning?",
+        "options": ["Företagsdrift", "Uppdrag att verkställa politiska beslut", "Ansvar för nationens ekonomi", "Ansvar för att skapa lagar"],
+        "answer": 1
+    },
+    {
+        "question": "Hur styrs våra myndigheter?",
+        "options": ["Av Kungen", "Av regeringen och riksdagen", "Av kommunerna", "Av EU"],
+        "answer": 1
+    },
+    {
+        "question": "Hur många kommuner och regioner har vi i Sverige?",
+        "options": ["290 kommuner och 21 regioner", "200 kommuner och 20 regioner", "300 kommuner och 25 regioner", "280 kommuner och 19 regioner"],
+        "answer": 0
+    },
+    {
+        "question": "Beskriv hur politik, juridik och förvaltning hör ihop.",
+        "options": ["Politik bestämmer lagarna, juridik tolkar dem, förvaltning verkställer dem", "Juridik bestämmer lagarna, politik tolkar dem, förvaltning verkställer dem", "Förvaltning bestämmer lagarna, juridik tolkar dem, politik verkställer dem", "Juridik bestämmer lagarna, förvaltning tolkar dem, politik verkställer dem"],
+        "answer": 0
+    },
+    {
+        "question": "Beskriv några viktiga skillnader mellan offentlig sektor och privat sektor?",
+        "options": ["Offentlig sektor finansieras av skatter, privat sektor av vinst", "Offentlig sektor styrs av regeringen, privat sektor av kunderna", "Offentlig sektor ägs av staten, privat sektor ägs av aktieägare", "Alla ovanstående"],
+        "answer": 3
+    },
+    {
+        "question": "Vad är en norm?",
+        "options": ["En lag", "En tradition", "Ett oskrivet samhällsregel", "Ett kontraktsförhållande"],
+        "answer": 2
+    },
+    {
+        "question": "Vilka är våra grundlagar?",
+        "options": ["Brottsbalken, Skollagen, Arbetsmiljölagen", "Regeringsformen, Successionsordningen, Tryckfrihetsförordningen, Yttrandefrihetsgrundlagen", "Brottsbalken, Skollagen, Tryckfrihetsförordningen, Yttrandefrihetsgrundlagen", "Regeringsformen, Brottsbalken, Skollagen, Arbetsmiljölagen"],
+        "answer": 1
+    },
+    {
+        "question": "Vilka fri- och rättigheter är grundlagsskyddade?",
+        "options": ["Rätt till arbete, rätt till bostad, rätt till utbildning", "Rätt till yttrandefrihet, tryckfrihet, religionsfrihet", "Rätt till arbete, rätt till yttrandefrihet, tryckfrihet", "Rätt till bostad, rätt till utbildning, religionsfrihet"],
+        "answer": 1
+    },
+    {
+        "question": "Hur ändrar man en grundlag?",
+        "options": ["Genom enkel majoritet i riksdagen", "Genom enkel majoritet i regeringen", "Genom två på varandra följande beslut av riksdagen med ett val emellan", "Genom folkomröstning"],
+        "answer": 2
+    },
+    {
+        "question": "Ge exempel på tre grundlagsskyddade fri- och rättigheter.",
+        "options": ["Rätt till arbete, rätt till bostad, rätt till utbildning", "Rätt till yttrandefrihet, tryckfrihet, religionsfrihet", "Rätt till arbete, rätt till yttrandefrihet, tryckfrihet", "Rätt till bostad, rätt till utbildning, religionsfrihet"],
+        "answer": 1
+    },
+    {
+        "question": "Ge exempel på tjänster och produkter som upphandlas inom offentlig verksamhet.",
+        "options": ["Hotellnätter, försäkringar, kontorsmaterial", "Lyxbilar, privatflyg, lyxyachter", "Privatbostäder, privatjordbruk, privatläkare", "Personliga gåvor, underhållning, alkohol"],
+        "answer": 0
+    },
+    {
+        "question": "Vad innebär offentlighetsprincipen?",
+        "options": ["Allmänheten har rätt att ta del av myndigheters handlingar", "Allmänheten har rätt att delta i politiska möten", "Allmänheten har rätt att rösta i allmänna val", "Allmänheten har rätt att kritisera regeringen"],
+        "answer": 0
+    },
+    {
+        "question": "Vad betyder meddelarfrihet? I vilken lag regleras det?",
+        "options": ["Rätten att tala fritt i media, regleras i Tryckfrihetsförordningen", "Rätten att tala fritt i media, regleras i Yttrandefrihetsgrundlagen", "Rätten att ge information till media om missförhållanden på arbetsplatsen, regleras i Tryckfrihetsförordningen", "Rätten att ge information till media om missförhållanden på arbetsplatsen, regleras i Yttrandefrihetsgrundlagen"],
+        "answer": 2
+    },
+    {
+        "question": "Vad innebär förbudet mot ministerstyre som vi har i Sverige?",
+        "options": ["Ministrar får inte ingripa i enskilda ärenden hos myndigheterna", "Ministrar får inte ingripa i politiska debatter", "Ministrar får inte ingripa i riksdagens arbete", "Ministrar får inte ingripa i kungens arbete"],
+        "answer": 0
+    },
+    {
+        "question": "Vilka principer är utgångspunkten för värdegrunden i offentlig förvaltning?",
+        "options": ["Demokrati, laglighet, effektivitet, objektivitet, likhet, transparens", "Marknadsstyrning, vinstmaximering, konkurrens, effektivitet", "Tradition, konsensus, homogenitet, stabilitet", "Innovation, risktagande, dynamik, kreativitet"],
+        "answer": 0
+    },
+    {
+        "question": "Beskriv vad kraven på effektivitet och service innebär i praktiken.",
+        "options": ["Myndigheter ska sträva efter att använda resurser på bästa sätt och ge god service till medborgarna", "Myndigheter ska minimera kostnader oavsett konsekvenserna", "Myndigheter ska maximera intäkter oavsett konsekvenserna", "Myndigheter ska fokusera på att bygga en stark organisation oavsett kostnader"],
+        "answer": 0
+    },
+    {
+        "question": "Beskriv vad kraven på objektivitet och saklighet och likhetskrav innebär i praktiken.",
+        "options": ["Myndigheter ska behandla alla ärenden och personer lika och basera beslut på fakta", "Myndigheter ska behandla ärenden och personer baserat på deras sociala status", "Myndigheter ska behandla ärenden och personer baserat på politiska preferenser", "Myndigheter ska behandla ärenden och personer baserat på personliga relationer"],
+        "answer": 0
+    },
+    {
+        "question": "Beskriv vad likhetskrav innebär i praktiken.",
+        "options": ["Alla ska behandlas lika oavsett personliga förhållanden", "Alla ska behandlas lika baserat på social status", "Alla ska behandlas lika baserat på politiska preferenser", "Alla ska behandlas lika baserat på personliga relationer"],
+        "answer": 0
+    },
+    {
+        "question": "Beskriv vad kravet på respekt innebär i praktiken.",
+        "options": ["Alla människor ska behandlas med värdighet och respekt", "Respekt krävs endast i hierarkiska relationer", "Respekt ska endast ges till dem med hög social status", "Respekt ska endast ges till dem med politiska kontakter"],
+        "answer": 0
+    },
+    {
+        "question": "Varför är kravet på transparens så viktigt?",
+        "options": ["För att skapa förtroende och möjliggöra granskning av offentliga handlingar", "För att skydda statens hemligheter", "För att undvika kritik", "För att skapa konkurrens mellan myndigheter"],
+        "answer": 0
+    },
+    {
+        "question": "Ge exempel på ett yrke/yrkeskår som omfattas av särskilda sekretessregler.",
+        "options": ["Läkare", "Journalister", "Politiker", "Kockar"],
+        "answer": 0
+    },
+    {
+        "question": "Beskriv begreppet samhällsnytta?",
+        "options": ["Något som gynnar samhället som helhet", "Något som gynnar en individ", "Något som gynnar en specifik grupp", "Något som gynnar företag"],
+        "answer": 0
+    },
+    {
+        "question": "Vad innebär begreppet respekt och service?",
+        "options": ["Att visa hänsyn och hjälpa andra", "Att dominera och styra andra", "Att ignorera andra och fokusera på egna mål", "Att manipulera andra för personlig vinning"],
+        "answer": 0
+    },
+    {
+        "question": "Vad innebär begreppen legalitet, objektivitet?",
+        "options": ["Att följa lagen och behandla alla ärenden och personer på samma sätt", "Att bryta lagen för att uppnå personliga mål", "Att behandla ärenden och personer baserat på personliga preferenser", "Att behandla ärenden och personer baserat på social status"],
+        "answer": 0
+    }
+]
 ];
 
 let score = 0;
+let currentStage = null;
 let currentQuestion = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
-    shuffleQuestions();
-    showQuestion();
+    showMenu();
 });
 
-function shuffleQuestions() {
+function showMenu() {
+  let stagesElement = document.getElementById("stages");
+  
+  stages.forEach((stage, index) => {
+    let button = document.createElement("button");
+    button.textContent = "Stage " + (index + 1);
+    button.addEventListener('click', () => startStage(index));
+    stagesElement.appendChild(button);
+  });
+}
+
+function startStage(index) {
+  currentStage = stages[index];
+  shuffleQuestions(currentStage);
+  showQuestion(currentStage);
+  
+  // hide the menu and show the quiz section
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("quiz-section").style.display = "block";
+}
+
+function shuffleQuestions(questions) {
     // Randomize the options and update the answer index
     questions.forEach(question => {
         let correctOption = question.options[question.answer];
@@ -277,9 +480,9 @@ function shuffleQuestions() {
     questions.sort(() => Math.random() - 0.5);
 }
 
-function showQuestion() {
+function showQuestion(questions) {
     if (currentQuestion >= questions.length) {
-        showFinalScore();
+        showFinalScores(currentStage);
         return;
     }
 
@@ -292,33 +495,33 @@ function showQuestion() {
     questions[currentQuestion].options.forEach((option, index) => {
         let button = document.createElement("button");
         button.textContent = option;
-        button.addEventListener('click', () => checkAnswer(index));
+        button.addEventListener('click', () => checkAnswer(index, currentStage));
         optionsElement.appendChild(button);
     });
 
-    showScore();
+    showScore(currentStage);
 }
 
-function checkAnswer(index) {
+function checkAnswer(index, questions) {
     if (index === questions[currentQuestion].answer) {
         alert('Yes! Correct answer: ' + questions[currentQuestion].answer);
         score++;
         currentQuestion++;
-        showQuestion();
+        showQuestion(currentStage);
     } else {
         alert('No! Wrong: ' + questions[currentQuestion].answer);
         if (score > 0)
           score--;
-        showQuestion();
+        showQuestion(currentStage);
     }
 }
 
-function showScore() {
+function showScore(questions) {
     let scoreElement = document.getElementById("score");
     scoreElement.textContent = `Nr ${currentQuestion}/${questions.length}  ------  Score: ${score}`;
 }
 
-function showFinalScore() {
+function showFinalScore(questions) {
     let quizElement = document.getElementById("quiz");
     quizElement.innerHTML = `<h2>Game Over! Your final score is: ${score}/${questions.length}</h2>
     <button onclick="restartGame()">Restart Game</button>`;
@@ -327,6 +530,4 @@ function showFinalScore() {
 function restartGame() {
     score = 0;
     currentQuestion = 0;
-    shuffleQuestions();
-    showQuestion();
 }
